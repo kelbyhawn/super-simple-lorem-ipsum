27722b68b0d66738ce4c25114faf0ff12545a94f
Kelby Hawn <kelbyhawn@gmail.com>
2018-01-01 13:17:57 -0800

    added files

---
+++ js/copyright.js
@@ -0,0 +1,5 @@
+var copyright = new Date();
+var year = copyright.getFullYear();
+
+var el = document.getElementById('footer');
+el.innerHTML = '<p>&copy;' + year + ' ' + 'Super Simple Lorem Ipsum <span style="color:#e2e2e2">//</span>' + ' ' + 'Developed by <a href="http://kelbyhawn.com" target="_blank">Kelby Hawn</a></p>'; 
