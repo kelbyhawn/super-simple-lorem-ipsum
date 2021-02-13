0592caf42477829a02bd5db8ec141d20df4e39d3
Kelby Hawn <kelbyhawn@gmail.com>
2021-02-13 11:01:52 -0800

    2021 update

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A no-frills Lorem Ipsum resource
 
-[supersimpleloremipsum.com/](http://supersimpleloremipsum.com/)
+[supersimpleloremipsum.com/](https://supersimpleloremipsum.com/)
--- js/copyright.js
+++ js/copyright.js
@@ -1,5 +1,3 @@
-var copyright = new Date();
-var year = copyright.getFullYear();
+let year = new Date().getFullYear();
 
-var el = document.getElementById('footer');
-el.innerHTML = '<p>&copy;' + year + ' ' + 'Super Simple Lorem Ipsum <span style="color:#e2e2e2">//</span>' + ' ' + 'Developed by <a href="http://kelbyhawn.com" target="_blank">Kelby Hawn</a></p>'; 
+document.querySelector('footer').innerHTML = `<p>&copy;${year} Super Simple Lorem Ipsum <span style="color:#e2e2e2">&bull;</span> Made by <a href="https://kelbyhawn.com" target="_blank">Kelby Hawn</a></p>`; 
