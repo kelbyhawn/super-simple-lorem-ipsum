a54e074820b8dc225c7dee131af794a6ead48710
kelbyhawn <9256643+kelbyhawn@users.noreply.github.com>
2026-07-23 14:04:36 -0700

    clean up button

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,11 +4,7 @@ import iconSun from "../assets/icon-sun.svg";
 
 export default function Button({ theme, onClick }) {
   return (
-    <button
-      id="btn-theme"
-      aria-label="Change to light or dark mode"
-      onClick={onClick}
-    >
+    <button aria-label="Change theme" onClick={onClick}>
       <Image
         src={theme === "light" ? iconMoon : iconSun}
         alt=""
