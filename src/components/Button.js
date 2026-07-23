fff36a613d0565eaa435521c7eec4b964b7d4c25
kelbyhawn <9256643+kelbyhawn@users.noreply.github.com>
2026-07-23 11:24:13 -0700

    refactor Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import Image from "next/image";
+import iconMoon from "../assets/icon-moon.svg";
+import iconSun from "../assets/icon-sun.svg";
+
 export default function Button() {
+  const [isLightTheme, setIsLightTheme] = useState(true);
+
+  const handleClick = () => setIsLightTheme(!isLightTheme);
+
   return (
-    <button id="btn-theme" aria-label="Change to light or dark mode"></button>
+    <button
+      id="btn-theme"
+      aria-label="Change to light or dark theme"
+      onClick={handleClick}
+    >
+      <Image
+        src={isLightTheme ? iconMoon : iconSun}
+        alt={isLightTheme ? "Moon icon" : "Sun icon"}
+        width={12}
+        height={12}
+      />
+      {isLightTheme ? "Dark" : "Light"}
+    </button>
   );
 }
